8ee9499b664e7b916873e1edea4094ac5b31c543
Raul Vinícius <165701907+tenshiiu@users.noreply.github.com>
2024-08-06 17:19:32 -0300

    Personalização do modal e inputs

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,7 @@ import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styled";
 import * as Dialog from "@radix-ui/react-dialog";
 
 import LogoImg from "../../assets/Logo.svg";
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header() {
     return (
@@ -15,14 +16,7 @@ export function Header() {
                 <NewTransactionButton>Nova Transação</NewTransactionButton>
             </Dialog.Trigger>
 
-            <Dialog.Portal>
-                <Dialog.Overlay />
-                <Dialog.Content>
-                    <Dialog.Title>Nova Transação</Dialog.Title>
-
-                    <Dialog.Close />
-                </Dialog.Content>
-            </Dialog.Portal>
+            <NewTransactionModal />
 
             </Dialog.Root>
          </HeaderContent>
